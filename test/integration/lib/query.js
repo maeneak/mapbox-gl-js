8518d17722e470ee94bf17a095d23ac60c14e2d4
Alexander Shalamov <alexander.shalamov@mapbox.com>
2019-06-18 13:39:24 +0300

    The diff module does not have default export, import diffJson explicitly

--- test/integration/lib/query.js
+++ test/integration/lib/query.js
@@ -1,6 +1,6 @@
 import path from 'path';
 import fs from 'fs';
-import diff from 'diff';
+import { diffJson } from 'diff';
 import {PNG} from 'pngjs';
 import harness from './harness';
 
@@ -65,7 +65,7 @@ export function run(implementation, options, query) {
             params.ok = deepEqual(results, expected);
 
             if (!params.ok) {
-                const msg = diff.diffJson(expected, results)
+                const msg = diffJson(expected, results)
                     .map((hunk) => {
                         if (hunk.added) {
                             return `+ ${hunk.value}`;
